dd6325a61034572b19f84058bb4b7d9ea798336c
subratasubrata20011997 <subrata.bhattacharjee.cse.15@gmail.com>
2021-05-08 13:41:28 +0530

    #project workspace updated with worker service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as d3 from 'd3';
+import { HttpClient } from "@angular/common/http";
+import {Papa} from 'ngx-papaparse'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  constructor(){}
-  ngOnInit(){
-    
+export class AppComponent implements OnInit {
+  constructor(private papa : Papa,private http: HttpClient) {
+
   }
+  ngOnInit() {
+    this.http.get('assets/data/state_wise.csv', {responseType: 'text'})
+    .subscribe(data =>{
+      this.papa.parse(data,{
+        header:true,
+        dynamicTyping: true,
+        complete: result =>{
+          console.log(result.data)
+          console.log(Object.keys(result.data[0]))
+        }
+      })
+    })
+  }
+
 }
